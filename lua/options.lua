044e4c4f259f4db07397b34c96ba048acc62a3d9
rasbin <rasbingodarthapa@gmail.com>
2023-12-17 08:59:08 +0545

    set tabs and auto indent to 4 spaces

--- lua/options.lua
+++ lua/options.lua
@@ -1,2 +1,8 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+-- Set tabstop, shiftwidth, and use spaces instead of tabs
+vim.api.nvim_set_option('tabstop', 4) -- number of spaces that a <tab> uses
+vim.api.nvim_set_option('shiftwidth', 4) -- number of spaces to use for (auto) indent
+vim.api.nvim_set_option('expandtab', true) -- use spaces instead of tabs
+
