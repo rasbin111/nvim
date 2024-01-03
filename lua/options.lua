3d27b27d1e061783a187798f0544ed526fba1e50
rasbin <rasbingodarthapa@gmail.com>
2024-01-03 20:19:25 +0545

    tab size 2 set for js files

--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,14 @@ vim.api.nvim_set_option('tabstop', 4) -- number of spaces that a <tab> uses
 vim.api.nvim_set_option('shiftwidth', 4) -- number of spaces to use for (auto) indent
 vim.api.nvim_set_option('expandtab', true) -- use spaces instead of tabs
 
+
+-- tab width to 2 spaces for js files 
+vim.api.nvim_exec([[
+  augroup JavaScriptTabSize
+    autocmd!
+    autocmd FileType javascript setlocal tabstop=2 shiftwidth=2 softtabstop=2
+  augroup END
+]], false)
+
+
+
