99e4af8bd0ea55a6a07756a229b1470301217bec
rasbin <rasbingodarthapa@gmail.com>
2024-01-05 18:01:38 +0545

    corrected the tab size to 4 by default

--- lua/options.lua
+++ lua/options.lua
@@ -2,11 +2,10 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Set tabstop, shiftwidth, and use spaces instead of tabs
-vim.api.nvim_set_option('tabstop', 4) -- number of spaces that a <tab> uses
-vim.api.nvim_set_option('shiftwidth', 4) -- number of spaces to use for (auto) indent
-vim.api.nvim_set_option('expandtab', true) -- use spaces instead of tabs
-
-
+vim.opt.tabstop = 4 -- number of spaces that <tab> uses
+vim.opt.shiftwidth = 4 -- number of spaces to use for auto indent
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.softtabstop = 4 -- Number of spaces for <Tab> key in insert and replace mode
 
 -- colorscheme
 vim.cmd("colorscheme tokyonight")
