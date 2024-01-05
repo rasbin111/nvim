901cce00f5624caeede6ed9a440822e58b56706d
rasbin <rasbingodarthapa@gmail.com>
2024-01-05 18:16:45 +0545

    commenting instructions added to init file

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ require("nvim-cmp")
 require("treesitter")
 
 -- comment
+-- "gcc" to comment a single line and "gc" to comment a block of code
+-- and "gcc" to uncomment a single line and "gc" to uncomment a 
+-- block of code.
 require("Comment").setup()
 
 
