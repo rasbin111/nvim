b64dd8c21943e22e5f6427c4af9aceceac2d473d
rasbin <rasbingodarthapa@gmail.com>
2025-08-29 10:26:33 +0545

    tabs and number added

---
+++ config/options.lua
@@ -0,0 +1,8 @@
+vim.opt.number = true 
+vim.opt.relativenumber = true 
+vim.opt.expandtab = true 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.mouse = "a" -- Enable mouse support in all modes
+vim.opt.ignorecase = true --Ignore case in searches 
+
