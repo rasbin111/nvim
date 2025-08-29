7c414a938a18fce35234065ad08fa96d73d5e3eb
rasbin <rasbingodarthapa@gmail.com>
2025-08-29 13:55:11 +0545

    added README.md

---
+++ README.md
@@ -0,0 +1,31 @@
+# Keymaps
+
+leader = <spacebar>
+
+## netrw file manager
+:Exp - file explorer 
+d - create a directory 
+% - create a file 
+
+move a file: 
+    - mt -> target directory 
+    - mf -> moving file / folder 
+    - mm -> execute move 
+
+# lsp 
+gd - go to defintion 
+gD - go to declaration 
+gi - go to implementation 
+gl - diagnostic 
+
+# telescope 
+leader ff - find files 
+leader fg - live grep 
+leader fb - find buffers 
+leader fh - find help
+
+
+# basics 
+:w - write 
+:q - quit 
+
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- lsp related 
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gl", function ()
+    vim.diagnostic.open_float(nil, {border = "rounded", focus = true})
+end)
+
