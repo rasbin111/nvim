e6f5507479fff01cb846f6b8f8de8e2da26e4d23
rasbin <rasbingodarthapa@gmail.com>
2026-08-05 19:25:58 +0545

    update snacks

--- lua/config/neo-tree.lua
+++ lua/config/neo-tree.lua
@@ -4,8 +4,6 @@ require("neo-tree").setup({
         statusline = false,
     },
     filesystem = {
-        -- Disables neo-tree from automatically opening when you run `nvim .`
-        hijack_netrw_behavior = "disabled",
         follow_current_file = {
             enabled = true,
         }
